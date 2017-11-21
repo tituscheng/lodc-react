2a87069967fe929885e85d7b4edc8fa39ed14f04
Titus Cheng <tituscheng@Tituss-MBP.T-mobile.com>
2017-11-21 01:23:01 -0800

    Api to amazon

--- src/components/model/Api.js
+++ src/components/model/Api.js
@@ -1,6 +1,6 @@
 export default class LodcAPI {
   static url() {
-    // return "http://default-environment.tdtddkdkmp.us-west-2.elasticbeanstalk.com/api/api";
-    return "http://localhost:9000/api/";
+    return "http://default-environment.tdtddkdkmp.us-west-2.elasticbeanstalk.com/api/api";
+    // return "http://localhost:9000/api/";
   }
 }
